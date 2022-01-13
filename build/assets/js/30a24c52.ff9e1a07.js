dde96d99753867ffcfb9b443672a454927e71e93
Anmol Malik <anmolmalik97new@gmail.com>
2022-01-13 17:47:48 +0530

    fix(*): remove build

--- build/assets/js/30a24c52.ff9e1a07.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetahos_guide=self.webpackChunkmetahos_guide||[]).push([[453],{8605:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/hello","name":"hello","count":2,"permalink":"/blog/tags/hello"}')}}]);
